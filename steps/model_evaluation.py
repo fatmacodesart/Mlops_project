9d86bd8d36f26bc1866a22c6ce61266fb228e7ea
fatmacodesart <fatma.ridaoui@gmail.com>
2024-03-26 19:58:34 +0300

    Add model evaluation step to evaluate_model function

--- steps/model_evaluation.py
+++ steps/model_evaluation.py
@@ -1,7 +1,35 @@
 import logging
+from typing import Tuple
+
 from zenml import step
 import pandas as pd
+from typing_extensions import Annotated
+
+from src.evaluation import MSE, R2, RMSE
+from sklearn.base import RegressorMixin
+
 
 @step
-def evaluate_model(data: pd.DataFrame) -> None:
-    pass
+def evaluate_model(model: RegressorMixin,
+                   X_test: pd.DataFrame,
+                   y_test: pd.DataFrame
+                   ) -> Tuple[Annotated[float, "R2 Score"],
+                              Annotated[float, "RMSE"],
+                              ]:
+    """Evaluate the model."""
+    try:
+        predictions = model.predict(X_test)
+        mse = MSE()
+        mse_score = mse.calculate_score(y_test, predictions)
+
+        r2 = R2()
+        r2_score = r2.calculate_score(y_test, predictions)
+
+        rmse = RMSE()
+        rmse_score = rmse.calculate_score(y_test, predictions)
+
+        return r2_score, rmse
+
+    except Exception as e:
+        logging.error(f"Error in evaluating model: {e}")
+        return
