b7b2c56252a4c161bf34cdc01dad40704a52cb51
fatmacodesart <fatma.ridaoui@gmail.com>
2024-03-26 11:28:00 +0300

    add train model

--- steps/model_training.py
+++ steps/model_training.py
@@ -1,7 +1,22 @@
 import logging
+
 import pandas as pd
 from zenml import step
 
-def train_model(data: pd.DataFrame) -> None:
-    """Trains a model"""
-    pass
+from src.model_dev import LinearRegressionModel
+from sklearn.base import RegressorMixin #for reg output
+
+from .config import ModelNameConfig
+
+@step
+def train_model(X_train: pd.DataFrame,
+                y_train: pd.DataFrame,
+                config: ModelNameConfig ) -> RegressorMixin:
+    """Trains a model and returns regressor output for evaluation."""
+    model = None
+    if config.model_name == 'LinearRegression':
+        model = LinearRegressionModel()
+        trained_model = model.train(X_train, y_train)
+        return trained_model
+    else:
+        raise ValueError(f"Model {config.model_name} not supported.") 
