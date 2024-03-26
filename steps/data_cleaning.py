ab11ef91c37a49e57d22b58b151bbfbd7b6e362c
fatmacodesart <fatma.ridaoui@gmail.com>
2024-03-26 19:58:27 +0300

    Refactor clean_data function in data_cleaning.py

--- steps/data_cleaning.py
+++ steps/data_cleaning.py
@@ -1,17 +1,19 @@
 import logging
 import pandas as pd
 from zenml import step
+
 from typing import Tuple
 from typing_extensions import Annotated
 
-from src.data_cleaning import DataPreprocessStrategy, DataSplitStrategy, DataCleaning 
+from src.data_cleaning import DataPreprocessStrategy, DataSplitStrategy, DataCleaning
+
 
 @step
 def clean_data(data: pd.DataFrame) -> Tuple[
     Annotated[pd.DataFrame, "X_train"],
     Annotated[pd.DataFrame, "X_test"],
     Annotated[pd.Series, "y_train"],
-    Annotated[pd.Series, "y_test"]
+    Annotated[pd.Series, "y_test"],
 ]:
     """
     Cleans data and returns training and testing data.
@@ -23,7 +25,7 @@ def clean_data(data: pd.DataFrame) -> Tuple[
         y_train (pd.Series): Training labels
         y_test (pd.Series): Testing labels
     """
-    
+
     try:
         preprocess_strategy = DataPreprocessStrategy()
         data_cleaning = DataCleaning(data, preprocess_strategy)
@@ -31,8 +33,10 @@ def clean_data(data: pd.DataFrame) -> Tuple[
 
         split_strategy = DataSplitStrategy()
         data_cleaning = DataCleaning(preprocessed_data, split_strategy)
-        X_train, X_test, y_train, y_test = data_cleaning.handle_data()
-        logging.info(f"Data cleaning completed. X_train shape: {X_train.shape}, X_test shape: {X_test.shape}")
+        x_train, x_test, y_train, y_test = data_cleaning.handle_data()
+        logging.info(
+            f"Data cleaning completed.")
+        return x_train, x_test, y_train, y_test
     except Exception as e:
         logging.error(f"Error in data cleaning: {e}")
         raise e
