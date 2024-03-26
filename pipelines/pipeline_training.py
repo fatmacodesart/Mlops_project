8258d0425c6536e5b13dbcb7c7509dd63f64fd18
fatmacodesart <fatma.ridaoui@gmail.com>
2024-03-26 19:58:10 +0300

    Refactor training pipeline to include data cleaning, model training, and evaluation

--- pipelines/pipeline_training.py
+++ pipelines/pipeline_training.py
@@ -4,11 +4,11 @@ from steps.data_cleaning import clean_data
 from steps.model_training import train_model
 from steps.model_evaluation import evaluate_model
 
-@pipeline(enable_cache=True)
 
+@pipeline(enable_cache=True)
 def train_pipeline(data_path: str):
     """Simple training pipeline"""
     df = ingest_data(data_path)
-    clean_data(df)
-    train_model(df)
-    evaluate_model(df)
+    X_train, X_test, y_train, y_test = clean_data(df)
+    trained_model = train_model(X_train, y_train)
+    r2_score , rmse_score = evaluate_model(trained_model, X_test, y_test)
